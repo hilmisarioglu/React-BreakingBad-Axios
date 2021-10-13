1d79ee5e1217e187cb525b1028b589705722207d
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-13 23:54:44 +0200

    v3

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from './components/Cards';
+import data from './data.json'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = 'App'>
+      <header>
+        <img src = 'https://images.wallpapersden.com/image/download/breaking-bad-criminal-elements_a2pmZ2WUmZqaraWkpJRnbGhnrWdlaW0.jpg' alt="Breaking Bad "/>
       </header>
+
+      <div className="cards">
+            {/* {data.map((item)=>(
+              <Card key={item.char_id} item={item.name} />
+            ))} */}
+            <Cards/>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
